ffae0da7bd812af4b7760249b7085feb23562d4d
DcCoO <dcc4@cin.ufpe.br>
2018-02-06 16:18:49 -0200

    ctrl funcionando com spin, spin ainda incorreto

--- Assets/Ctrl.cs
+++ Assets/Ctrl.cs
@@ -8,12 +8,14 @@ public class Ctrl : MonoBehaviour {
 	Transform camera;
 	float minDist = 1.7f;
 	float maxDist = 5;
-	float mouseConstant = 0.1f;
+	float mouseConstant = 0.2f;
 	float mouseY;
 
 	Vector2 spin;
 
-	float distance = 2;
+	float distance;
+	float nextDistance;
+
 	// Use this for initialization
 	void Start () {
 		camera = GameObject.Find ("Main Camera").transform;
@@ -27,21 +29,63 @@ public class Ctrl : MonoBehaviour {
 	void Update () {
 		if (!turnedOn) return;
 
-		distance = Mathf.Clamp(distance - Input.GetAxis("Mouse Y") * 0.48f, minDist, maxDist);
+		nextDistance = Mathf.Clamp(distance - Input.GetAxis("Mouse Y") * mouseConstant, minDist, maxDist);
+		print ("DISTANCE = " + distance + "      OFFSET = " + (Input.GetAxis("Mouse Y") * mouseConstant));
 
-		Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-		Vector3 position = transform.rotation * negDistance + cueBall.position;
-
-		transform.position = position;
-		transform.LookAt (cueBall.position + 
-			transform.right * spin.x * 0.07f +
-			transform.up    * spin.y * 0.07f
-		);
+		transform.position -= transform.forward * (nextDistance - distance);
+		distance = nextDistance;
 
 	}
 
 	public void Activate(bool state){
-		if(!turnedOn && state) this.mouseY = mouseY;
+		if(!turnedOn && state) distance = 2.1f;
 		turnedOn = state;
 	}
 }
+
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour {
+
+	public Transform cueBall;
+	Transform camera;
+	float minDist = 1.7f;
+	float maxDist = 5;
+	float mouseConstant = 0.2f;
+	float mouseY;
+
+	Vector2 spin;
+
+	float distance;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("Main Camera").transform;
+		spin = GetComponent<Cue> ().spin;
+	}
+
+	Vector3 velocity = Vector3.zero;
+
+	public bool turnedOn = false;
+	// Update is called once per frame
+	void Update () {
+		if (!turnedOn) return;
+
+		distance = Mathf.Clamp(distance - Input.GetAxis("Mouse Y") * mouseConstant, minDist, maxDist);
+		print ("DISTANCE = " + distance + "      OFFSET = " + (Input.GetAxis("Mouse Y") * mouseConstant));
+
+		transform.position -= transform.forward * (distance - Vector3.Distance (transform.position, cueBall.position));
+
+	}
+
+	public void Activate(bool state){
+		if(!turnedOn && state) distance = 2.1f;
+		turnedOn = state;
+	}
+}
+*/
